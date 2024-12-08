51f27361cc179b475058ef6208d79e5b25b2c62d
Lngngyn289 <longnguyenduc98921@gmail.com>
2024-12-08 17:03:00 +0700

    k66 treasures problems

--- Practice/ex3_midterm.cpp
+++ Practice/ex3_midterm.cpp
@@ -1,21 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define MOD 1e9+7; 
+const int MOD = 1e9+7; 
 int n;
 map<int, int> mp;
 int res = 0;
 
+//XB
+// int main(){
+//   cin >> n;
+//   while(n--){
+//     int x;
+//     cin >> x;
+//     mp[x]++;
+//   }
+//   for(map<int,int>::iterator it = mp.begin(); it != mp.end(); it++){
+//     int k = (*it).second;
+//     res += k*(k-1)/2;
+//      res %= MOD;
+//   }
+//   cout << res;
+// }
+
+
+//BL
 int main(){
   cin >> n;
   while(n--){
-    int x;
-    cin >> x;
+    int x; cin >> x;
     mp[x]++;
   }
-  for(map<int,int>::iterator it = mp.begin(); it != mp.end(); it++){
-    int k = (*it).second;
-    res += k*(k-1)/2;
+  map<int,int>::iterator it = mp.begin();
+  int prv = (*it).second;
+  it++;
+  for(; it != mp.end(); it++){
+    int cur = (*it).second;
+    if(cur - prv == 1){
+      res += max(prv, cur);
+      res %= MOD;
+    }
+    prv = cur;
   }
   cout << res;
 }
--- Practice/ex4_midterm.cpp
+++ Practice/ex4_midterm.cpp
@@ -1,6 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-  
+int n, m, ok = 0, res = 0 ;
+int hx[3] = {0, 1, 1};
+int hy[3] = {1, 0, 1};
+int a[1001][1001];
+
+void Try(int i, int j, int sum){
+  if(i == n && j == m){
+    ok = 1;
+    res = min(sum, res);
+    return;
+  }
+  if(i + 1 <= n && a[i+1][j] != 0){
+    Try(i+1,j, sum + a[i+1][j]);
+  }
+  if(j + 1 <= m && a[i][j+1] != 0){
+    Try(i,j+1, sum + a[i][j+1]);
+  }
+  if(j + 1 <= m && i + 1 <= n && a[i+1][j+1] != 0){
+    Try(i+1,j+1, sum + a[i+1][j+1]);
+  }
 }
+
+int main(){
+  cin >> n >> m;
+  for(int i = 1; i <= n; i++){
+    for(int j = 1; j <= m; j++){
+      cin >> a[i][j];
+    }
+  }
+  Try(1,1,a[1][1]);
+  if(!ok) cout << "-1";
+  else cout << res;
+}
+
+// 5 6
+// 1 2 0 5 1 3
+// 5 -9 2 0 1 3
+// 1 4 1 5 7 4
+// -60 1 0 0 0 0
+// 6 7 0 5 4 3
+
+// 5 6
+// 1 2 0 5 1 3
+// 5 -9 2 0 1 3
+// 1 4 1 5 7 4
+// -60 1 9 0 29 0
+// 6 7 0 5 4 3
